86afdc2a3e179b5172d0af2b464426dde12f815d
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-08-29 18:23:22 -0400

    bug fix CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 // apply middleware (cors, parser)
 app.use(cors({ 
-  origin: [process.env.FRONTEND_URL, 'http://localhost:3000']
+  origin: process.env.FRONTEND_URL
   // credentials: true
 }));
 
