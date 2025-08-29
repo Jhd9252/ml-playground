5059241b5a2d58f0cb814aa0ed7eb56950aae4f9
Jon Dinh <87919907+jhd9252@users.noreply.github.com>
2025-08-29 16:27:42 -0400

    updated build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Install Node.js dependencies
+npm install
+
 # Install Python dependencies
 
 echo "Installing Python dependencies..."
 pip install -r ./ml-service/requirements.txt
+
